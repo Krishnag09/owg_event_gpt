9648f1c3da72a89803c97b37c81d4e43d1d6ff70
Krishnag09 <krishnag0902@gmail.com>
2024-04-08 21:39:03 -0400

    changes to context

--- routers/event.py
+++ routers/event.py
@@ -7,15 +7,14 @@ load_dotenv()
 router = APIRouter()
 
 
-@router.get("/desc", description="get test response", )
-async def expand():
+@router.get("/desc/{event_details}", description="get test response", )
+async def expand(event_details: str):
     client = OpenAI(api_key=os.environ.get("OPEN_API_KEY"))
     completion = client.chat.completions.create(model="gpt-3.5-turbo",
                                                 messages=[
                                                     {"role": "system",
-                                                     "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+                                                     "content": "You are a regular young professional, write 100-200 words encouraging people to join an outdoor event at provided venue and activities. Write plain and casual"},
                                                     {"role": "user",
-                                                     "content": "Compose a poem that explains the concept of recursion in programming."}
+                                                     "content": f"Write an event desc using {event_details}"}
                                                 ])
-    print(completion)
     return completion
